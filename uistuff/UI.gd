fc11440aa246a61c04f8a0a94af3329131b921b3
Nadine <83024576+Nadine-F@users.noreply.github.com>
2023-11-28 17:34:48 +0100

    Working Selection Wheel Added

---
+++ uistuff/UI.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+func _process(delta):
+	if Input.is_action_just_pressed("block_select"):
+		$SelectionWheel.show()
+	elif Input.is_action_just_released("block_select"):
+		var tool = $SelectionWheel.Close()
+		$Label.text = "Selected Block: " + tool
