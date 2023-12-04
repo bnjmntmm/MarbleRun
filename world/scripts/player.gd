50e001269772ad1e7f1d59c610eb53a18727dc41
timmno12 <benjamintimm21@gmail.com>
2023-12-04 16:09:59 +0100

    World Movement via Button press
    If all 4 triggers pressed -> moving arms makes world go move

--- world/scripts/player.gd
+++ world/scripts/player.gd
@@ -6,8 +6,16 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var viewport_2_din_3d = $Left/Viewport2Din3D
 @onready var label_3d = $XRCamera3D/Label3D
 @onready var left_controller = $Left
+@onready var right_controller = $Right
+
 @onready var right_hand = $Right/RightHand
 
+var grib_left = false
+var grib_right = false
+var trigger_left = false
+var trigger_right = false
+var allGrabbed = false
+var init_controller_pos : Vector3
 
 ## MARBLE TRACKS INIT
 var straightBlock = ResourceLoader.load("res://scenes/pickable_straight_normal.tscn")
@@ -30,12 +38,13 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	label_3d.text = str(selectionCircle.get_current_selection())
+	#label_3d.text = str(selectionCircle.get_current_selection())
 	get_joystick_input(XRHelpers.get_xr_controller(self))
 	var is_colliding = _process_on_physical_movement(delta)
 	if !is_colliding:
 		_process_rotation_on_input(delta)
 		_process_movement_on_input(delta)
+	change_world_pos()
 
 func _process_on_physical_movement(delta):
 	#current velocity
@@ -108,20 +117,44 @@ func _process_movement_on_input(delta):
 #func print_vr(text:String):
 #	debug_window.text=str(text)
 func _on_left_button_pressed(name):
+	#label_3d.text = name
 	#print_vr(name)
 	if name=="ax_button":
 		viewport_2_din_3d.visible = !viewport_2_din_3d.visible
 	if name=="by_button":
 		get_tree().reload_current_scene()
+	if name=="grip_click":
+		grib_left = true
+	if name=="trigger_click":
+		trigger_left = true
+		
+
+func _on_left_button_released(name):
+	if name=="grip_click":
+		grib_left = false
+	if name=="trigger_click":
+		trigger_left = false
+
+
+
 func _on_right_button_pressed(name):
-	
+	#label_3d.text = name
 	if name=="ax_button":
 		if selectionCircle.get_current_selection() == 1:
 			var block = straightBlock.instantiate()
 			get_parent().add_child(block)
 			block.global_position=right_hand.global_position
 			block.freeze=true
+	if name=="grip_click":
+		grib_right = true
+	if name=="trigger_click":
+		trigger_right = true
 
+func _on_right_button_released(name):
+	if name=="grip_click":
+		grib_right = false
+	if name=="trigger_click":
+		trigger_right = false
 			
 
 func get_joystick_input(_controller):
@@ -133,11 +166,25 @@ func get_joystick_input(_controller):
 		
 		
 		
-
-
+func change_world_pos():
+	var speed  = 2.0
+	if grib_left and grib_right and trigger_left and trigger_right:
+		var avg_pos = (left_controller.global_transform.origin + right_controller.global_transform.origin) / 2.0
+		if not allGrabbed:
+			allGrabbed = true
+			init_controller_pos = avg_pos
+		else:
+			#Calc diff between the positions
+			var delta_pos = Vector3(avg_pos.x - init_controller_pos.x, 0.0, avg_pos.z - init_controller_pos.z)
+			global_transform.origin -= delta_pos
+			init_controller_pos = avg_pos
+	else:
+		allGrabbed = false
 
 func spawn_track(spawningTrack):
 	var newBlock = spawningTrack.instantiante()
 	get_parent().add_child(newBlock)
 	newBlock.global_position = right_hand.global_position
 	newBlock.freeze = true
+
+
