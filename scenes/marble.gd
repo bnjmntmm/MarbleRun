23b039d3cba259e63773f8ea9dea37c2e493d838
dinkelhuber <wiemkenmika@googlemail.com>
2024-01-22 13:21:34 +0100

    small speed improvement

--- scenes/marble.gd
+++ scenes/marble.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 var boost := false
 var boost_factor := 2.0  # The factor by which the speed is increased
 var max_speed_boosted := 1.0  # Maximum speed limit when boosted
-var max_speed_normal := 2.0  # Maximum speed limit under normal conditions
+var max_speed_normal := 10.0  # Maximum speed limit under normal conditions
 
 func _integrate_forces(state):
 	var max_speed = max_speed_boosted if boost else max_speed_normal
